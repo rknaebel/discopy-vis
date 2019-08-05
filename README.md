908ed70e4e5db7b5df2b88cdbb829f4a461f7c13
rknaebel <rknaebel@uni-potsdam.de>
2019-08-05 08:43:13 +0200

    init visualization project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Visualizer for Shallow Discourse Parsers
+In this project we develop a framework for the visualization of discourse structures produced by a discourse parser in the CONLL format.
+The project provides a simple Flask service, which handles incoming parses and produces a simple website frontend.
+
+## Usage
+
+## Expected Format
+wip
+
---
+++ run_service.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+import argparse
+
+import flask
+from flask_cors import CORS
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--parser-ip", help="",
+                    default='127.0.0.1')
+parser.add_argument("--parser-port", help="",
+                    default='1234')
+args = parser.parse_args()
+
+# initialize our Flask application and the Keras model
+app = flask.Flask(__name__, template_folder='app')
+# TODO check why CORS!?
+CORS(app)
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # initialize the data dictionary that will be returned from the
+    # view
+    data = {"success": False}
+
+    # ensure an image was properly uploaded to our endpoint
+    if flask.request.method == "POST":
+        json_obj = flask.request.get_json()
+
+        # TODO do something with the raw text
+        data["text"] = json_obj['text']
+
+        # indicate that the request was a success
+        data["success"] = True
+
+    print(data)
+
+    # return the data dictionary as a JSON response
+    return flask.jsonify(data)
+
+
+@app.route("/")
+@app.route("/index")
+def main():
+    return flask.render_template('index.html', title='Home')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
